94870559956bf3a568acca27f19dced010aacad6
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-09-21 13:39:43 +0530

    Add files via upload

---
+++ stream.php
@@ -0,0 +1,78 @@
+<?php
+$p= @file_get_contents("http://jiorr.azurewebsites.net/token.php"); # Change This Path According to Your Server and Folder
+
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+# If Got Any Problem Meet me at @AvishkarPatil [ Telegram ]
+
+
+if(@$_REQUEST["key"]!="")
+{
+	$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: plaYtv/5.8.1 (Linux;Android 9) ExoPlayerLib/2.8.0\r\n" .
+		"lbcookie: 300\r\n" .
+"devicetype: Kodi\r\n" .
+"os: android\r\n" .
+"appkey: NzNiMDhlYzQyNjJm\r\n" .
+"deviceId: 2f5f4c6443fe0800\r\n" .
+"versionCode: 226\r\n" .
+"osVersion: 9\r\n" .
+"isott: true\r\n" .
+"languageId: 6\r\n" .
+"uniqueId: c1464eef-ffcf-4f1a-a594-20dba25e8266\r\n" . # Change uniqueId if you chnaged token
+"srno: 200206173037\r\n" .
+"usergroup: tvYR7NSNn7rymo3F\r\n" .
+"channelid: 472\r\n" .
+"ssotoken: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiJjMTQ2NGVlZi1mZmNmLTRmMWEtYTU5NC0yMGRiYTI1ZTgyNjYiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiI4ODAyNjZhY2I1NDcyYmJjYWYwZTY0ZWE2YmE0NWE2ZDM1ZTAxNzVkZWQ2NTk4OTIwNGM0NDRhNzlhN2U3MWM4MDA0YWVkNTJlMDgyYWYzN2JkYjk1NGJiNTYxYjM3YmMxOWViOWMzNWMwZmI2MWFjOWVlZjE2ZjM3OTMwOWExNiIsImp0aSI6IjIyNTcxZWQ0LWFjMDctNDdkYS04ZDBmLWU5ZDE4ZGRiY2FiMiIsImlhdCI6MTYzMjIxMTI0M30.L0VgT9wvMayuHqgEIcG10L1HUwcrsnBODszo8xoYG1I\r\n" #Change To Your Details
+	    
+    ]
+];
+
+
+$cache=str_replace("/","_",$_REQUEST["key"]);
+
+if(!file_exists($cache)){
+
+$context = stream_context_create($opts);
+$haystack = file_get_contents("https://tv.media.jio.com/streams_live/"  . $_REQUEST["key"] . $p,false,$context);
+
+}
+else
+{
+$haystack=file_get_contents($cache);
+
+}
+echo $haystack;
+}
+
+
+if(@$_REQUEST["ts"]!="")
+{
+header("Content-Type: video/mp2t");
+header("Connection: keep-alive");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Expose-Headers: Content-Length,Content-Range");
+header("Access-Control-Allow-Headers: Range");
+header("Accept-Ranges: bytes");
+	$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: plaYtv/5.3.2 (Linux;Android 5.1.1) ExoPlayerLib/2.3.0/2.3.0\r\n" 
+
+
+    ]
+];
+
+$context = stream_context_create($opts);
+$haystack = file_get_contents("http://mumsite.cdnsrv.jio.com/jiotv.live.cdn.jio.com/"  . $_REQUEST["ts"],false,$context);
+echo $haystack;
+
+}
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+# DON'T CHANGE ANY JIO SERVER LINK and USERAGENT
+# DON'T REMOVE CREDITS
+
+?>
---
+++ token.php
@@ -0,0 +1,41 @@
+<?php
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+# Here I Put Token which Available Publicly I Recommended Use Your Own Token Here 
+# For Suppport @Avishkarpatil at Telegram or proavipatil@gmail.com
+
+
+
+$jctBase = "cutibeau2ic";
+
+$ssoToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiJjMTQ2NGVlZi1mZmNmLTRmMWEtYTU5NC0yMGRiYTI1ZTgyNjYiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiI4ODAyNjZhY2I1NDcyYmJjYWYwZTY0ZWE2YmE0NWE2ZDM1ZTAxNzVkZWQ2NTk4OTIwNGM0NDRhNzlhN2U3MWM4MDA0YWVkNTJlMDgyYWYzN2JkYjk1NGJiNTYxYjM3YmMxOWViOWMzNWMwZmI2MWFjOWVlZjE2ZjM3OTMwOWExNiIsImp0aSI6IjIyNTcxZWQ0LWFjMDctNDdkYS04ZDBmLWU5ZDE4ZGRiY2FiMiIsImlhdCI6MTYzMjIxMTI0M30.L0VgT9wvMayuHqgEIcG10L1HUwcrsnBODszo8xoYG1I"; #Change This with your SSOTOKEN 
+function tokformat($str)
+{
+$str= base64_encode(md5($str,true));
+return str_replace("\n","",str_replace("\r","",str_replace("/","_",str_replace("+","-",str_replace("=","",$str)))));
+}
+function generateJct($st, $pxe) 
+{
+ global $jctBase;
+ return trim(tokformat($jctBase . $st . $pxe));
+}
+function generatePxe() {
+return time() + 6000000;
+}
+function generateSt() {
+global $ssoToken;
+return tokformat($ssoToken);
+}
+function generateToken() {
+$st = generateSt();
+$pxe = generatePxe();
+$jct = generateJct($st, $pxe);
+return "?jct=" . $jct . "&pxe=" . $pxe . "&st=" . $st;
+}
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+
+echo generateToken();
+?>
